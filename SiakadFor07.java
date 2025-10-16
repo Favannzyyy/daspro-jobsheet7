6a5767a123d5be8dece690c88938c49d6fe71283
Favannzyyy <favannzyyy@gmail.com>
2025-10-16 08:57:58 +0700

    Menambahkan jumlah lulus

--- SiakadFor07.java
+++ SiakadFor07.java
@@ -5,6 +5,7 @@ public class SiakadFor07 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int jmlLulus = 0, jmlTidakLulus = 0; 
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -17,10 +18,17 @@ public class SiakadFor07 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            if (nilai >= 60) {
+                jmlLulus++;       // tambah 1 ke jumlah lulus
+            } else {
+                jmlTidakLulus++;  // tambah 1 ke jumlah tidak lulus
+            }
         }
 
-        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("\nNilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + jmlLulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + jmlTidakLulus);
     }
 }
-
